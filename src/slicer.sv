d62409fafdffbb75223436ddfaeff7b06feeac41
VetaShine <111084369+VetaShine@users.noreply.github.com>
2025-12-17 13:11:26 +0300

    feat: update slicer.sv

--- src/slicer.sv
+++ src/slicer.sv
@@ -60,11 +60,11 @@ module slicer #(
 );
     logic [ADDR_WIDTH - 1 : 0]       aw_addr_0     ;
     logic [2 : 0]                    aw_prot_0     ;
-    logic [1 : 0]                    aw_fifo_status;
+    logic                            aw_fifo_status;
 
     logic [DATA_WIDTH - 1 : 0]       w_data_0	   ;
     logic [(DATA_WIDTH / 8) - 1 : 0] w_strb_0	   ;
-    logic [1 : 0]                    w_fifo_status ;
+    logic                            w_fifo_status ;
 
     logic [1 : 0]                    b_resp_0	   ;
     logic [1 : 0]                    b_fifo_status ;
@@ -77,7 +77,6 @@ module slicer #(
     logic [1 : 0]                    r_resp_0	   ;
     logic                            r_fifo_status ;
 
-    logic                            r_err_0	   ;
     logic                            aw_err_0	   ;
     logic                            w_err_0	   ;
     logic                            aw_stored     ;
@@ -86,36 +85,25 @@ module slicer #(
 
     assign aw_write_flag = s_axi_awvalid & s_axi_awready;
     assign aw_read_flag  = m_axi_awvalid & m_axi_awready;
-    assign aw_empty	     = (aw_fifo_status == 0);
-    assign aw_full	     = aw_fifo_status;
 
     assign w_write_flag  = s_axi_wvalid & s_axi_wready;
     assign w_read_flag   = m_axi_wvalid & m_axi_wready;
-    assign w_empty	     = (w_fifo_status == 0);
-    assign w_full        = w_fifo_status;
 
     assign b_write_flag  = m_axi_bvalid & m_axi_bready;
     assign b_read_flag   = s_axi_bvalid & s_axi_bready;
-    assign b_empty	     = (b_fifo_status == 0);
-    assign b_full        = b_fifo_status;
 
     assign ar_write_flag = s_axi_arvalid & s_axi_arready;
     assign ar_read_flag  = m_axi_arvalid & m_axi_arready;
-    assign ar_empty	     = (ar_fifo_status == 0);
-    assign ar_full	     = ar_fifo_status;
 
     assign r_write_flag  = m_axi_rvalid & m_axi_rready;
     assign r_read_flag   = s_axi_rvalid & s_axi_rready;
-    assign r_empty	     = (r_fifo_status == 0);
-    assign r_full        = r_fifo_status;
 
     assign write_accepted = m_axi_awvalid && m_axi_awready && m_axi_wvalid && m_axi_wready;
-    assign write_ready    = aw_stored && w_stored;
 
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
-            aw_fifo_status <= 2'b0;
+            aw_fifo_status <= 1'b0;
         end else if (aw_write_flag & ~aw_read_flag) begin
             aw_fifo_status <= 1'b1;
         end else if (b_read_flag) begin
@@ -125,7 +113,7 @@ module slicer #(
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
-            w_fifo_status <= 2'b0;
+            w_fifo_status <= 1'b0;
         end else if (w_write_flag & ~w_read_flag) begin
             w_fifo_status <= 1'b1;
         end else if (b_read_flag) begin
@@ -135,7 +123,7 @@ module slicer #(
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
-            b_fifo_status <= 2'b0;
+            b_fifo_status <= 1'b0;
         end else if (b_write_flag & ~b_read_flag) begin
             b_fifo_status <= 1'b1;
         end else if (~b_write_flag & b_read_flag) begin
@@ -145,17 +133,17 @@ module slicer #(
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
-            ar_fifo_status <= 2'b0;
+            ar_fifo_status <= 1'b0;
         end else if (ar_write_flag & ~ar_read_flag) begin
             ar_fifo_status <= 1'b1;
-        end else if (s_axi_rvalid & s_axi_rready) begin
+        end else if (r_read_flag) begin
             ar_fifo_status <= 1'b0;
         end
     end
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
-            r_fifo_status <= 2'b0;
+            r_fifo_status <= 1'b0;
         end else if (r_write_flag & ~r_read_flag) begin
             r_fifo_status <= 1'b1;
         end else if (~r_write_flag & r_read_flag) begin
@@ -163,21 +151,24 @@ module slicer #(
         end
     end
 
-    assign s_axi_awready = ~aw_full;
-    assign s_axi_wready  = ~w_full ;
-    assign m_axi_bready  = ~b_full ;
-    assign s_axi_arready = ~ar_full;
-    assign m_axi_rready  = ~r_full ;
+    assign s_axi_awready = ~aw_fifo_status;
+    assign s_axi_wready  = ~w_fifo_status ;
+    assign m_axi_bready  = ~b_fifo_status ;
+    assign s_axi_arready = ~ar_fifo_status;
+    assign m_axi_rready  = ~r_fifo_status ;
 
-    assign m_axi_awvalid = write_ready;
-    assign m_axi_wvalid  = write_ready;
+    assign m_axi_awvalid  = aw_stored & w_stored & ~aw_err_0 & ~w_err_0;
+    assign m_axi_wvalid   = aw_stored & w_stored & ~aw_err_0 & ~w_err_0;
+    assign bvalid_for_err = (err_awrite_i || err_write_i || aw_err_0 || w_err_0) & aw_stored & w_stored;
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
             s_axi_bvalid <= 1'b0;
-        end else if ((b_fifo_status == 1) & b_read_flag & ~b_write_flag) begin
+        end else if (b_read_flag & ~b_write_flag) begin
             s_axi_bvalid <= 1'b0;
-        end else if (b_empty & b_write_flag & ~b_read_flag) begin
+        end else if (b_write_flag & ~b_read_flag) begin
+            s_axi_bvalid <= 1'b1;
+        end else if (bvalid_for_err) begin
             s_axi_bvalid <= 1'b1;
         end
     end
@@ -187,7 +178,7 @@ module slicer #(
             m_axi_arvalid <= 1'b0;
         end else if ((ar_fifo_status == 1) & ar_read_flag & ~ar_write_flag) begin
             m_axi_arvalid <= 1'b0;
-        end else if (ar_empty & ar_write_flag & ~ar_read_flag) begin
+        end else if (ar_write_flag & ~ar_read_flag & ~err_read_i) begin
             m_axi_arvalid <= 1'b1;
         end
     end
@@ -195,9 +186,11 @@ module slicer #(
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
             s_axi_rvalid <= 1'b0;
-        end else if ((r_fifo_status == 1) & r_read_flag & ~r_write_flag) begin
+        end else if (r_read_flag & ~r_write_flag) begin
             s_axi_rvalid <= 1'b0;
-        end else if (r_empty & r_write_flag & ~r_read_flag) begin
+        end else if (r_write_flag & ~r_read_flag) begin
+            s_axi_rvalid <= 1'b1;
+        end else if (err_read_i & ar_write_flag) begin
             s_axi_rvalid <= 1'b1;
         end
     end
@@ -219,6 +212,8 @@ module slicer #(
             aw_stored <= 1'b1;
         end else if (write_accepted) begin
             aw_stored <= 1'b0;
+        end else if (bvalid_for_err) begin
+            aw_stored <= 1'b0;
         end
     end
 
@@ -229,6 +224,8 @@ module slicer #(
             w_stored <= 1'b1;
         end else if (write_accepted) begin
             w_stored  <= 1'b0;
+        end else if (bvalid_for_err) begin
+            w_stored <= 1'b0;
         end
     end
 
@@ -256,11 +253,13 @@ module slicer #(
         end
     end
 
-        always_ff @(posedge aclk or negedge aresetn) begin
+    always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
             b_resp_0 <= 2'b0;
         end else if (b_write_flag) begin
-            b_resp_0 <= (aw_err_0 || w_err_0) ? 2'b10 : m_axi_bresp;
+            b_resp_0 <= m_axi_bresp;
+        end else if (bvalid_for_err) begin
+            b_resp_0 <= 2'b10;
         end
     end
 
@@ -268,21 +267,25 @@ module slicer #(
         if (~aresetn) begin
             ar_addr_0 <= {ADDR_WIDTH{1'b0}};
             ar_prot_0 <= 3'b0;
-            r_err_0   <= 1'b0;
         end else if (ar_write_flag) begin
             ar_addr_0 <= s_axi_araddr;
             ar_prot_0 <= s_axi_arprot;
-            r_err_0   <= err_read_i;
         end
     end
 
     always_ff @(posedge aclk or negedge aresetn) begin
         if (~aresetn) begin
             r_data_0 <= {DATA_WIDTH{1'b0}};
-            r_resp_0 <= 2'b0;
         end else if (r_write_flag) begin
             r_data_0 <= m_axi_rdata;
-            r_resp_0 <= r_err_0 ? 2'b10 : m_axi_rresp;
-        end
+        end 
+    end
+
+    always_ff @(posedge aclk or negedge aresetn) begin
+        if (~aresetn) begin
+            r_resp_0 <= 2'b0;
+        end else if (r_write_flag || err_read_i) begin
+            r_resp_0 <= err_read_i ? 2'b10 : m_axi_rresp;
+        end 
     end
 endmodule
